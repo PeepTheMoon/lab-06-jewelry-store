cbf249c6fc88914126ffcec6af1afe588a46c84b
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-06 14:51:17 -0700

    added a data list of objects

--- Data/products.js
+++ Data/products.js
@@ -2,7 +2,73 @@ const ring = {
     id: 'ring',
     name: 'Statement Ring',
     image: 'ring.jpeg',
-    description: 'A simple statement ring featuring geometric shapes.',
+    description: 'A simple gold statement ring featuring geometric shapes.',
     category: 'rings',
     price: 54.00,
 };
+
+const earrings = {
+    id: 'earrings',
+    name: 'Face Earrings',
+    image: 'earrings.jpeg',
+    description: 'Funky face earrings in gold.',
+    category: 'earrings',
+    price: 50.00,
+};
+
+const bracelet = {
+    id: 'bracelet',
+    name: 'Charm Bracelet',
+    image: 'charm-bracelet.jpeg',
+    description: 'Gold charm bracelet with giraffe.',
+    category: 'bracelets',
+    price: 60.00,
+};
+
+const necklace = {
+    id: 'necklace',
+    name: 'Crystal Necklace',
+    image: 'crystal-necklace.jpeg',
+    description: 'Gold necklace with crystal adornments.',
+    category: 'necklaces',
+    price: 70.00,
+};
+
+const nameplateNecklace = {
+    id: 'nameplate necklace',
+    name: 'Nameplate Necklace',
+    image: 'nameplate-necklace.jpeg',
+    description: 'Gold necklace with name.',
+    category: 'necklaces',
+    price: 60.00,
+};
+
+const noseRing = {
+    id: 'nose ring',
+    name: 'Nose Ring',
+    image: 'nose-ring.jpeg',
+    description: 'A simple gold nose ring.',
+    category: 'body jewelry',
+    price: 30.00,
+};
+
+const watch = {
+    id: 'watch',
+    name: 'Watch',
+    image: 'watch.jpeg',
+    description: 'A gold watch.',
+    category: 'watches',
+    price: 110.00,
+};
+
+const jewelry = [
+    ring,
+    earrings,
+    bracelet,
+    necklace,
+    nameplateNecklace,
+    noseRing,
+    watch
+];
+
+export default jewelry;
