fcb33d2be0adf1a9b894db5f1b4a446d672baacb
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-06 14:20:08 -0700

    works out basic design structure and adds a product with key value pairs.

---
+++ Data/products.js
@@ -0,0 +1,8 @@
+const ring = {
+    id: 'ring',
+    name: 'Statement Ring',
+    image: 'ring.jpeg',
+    description: 'A simple statement ring featuring geometric shapes.',
+    category: 'rings',
+    price: 54.00,
+};
