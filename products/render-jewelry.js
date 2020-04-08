0976d220428952bc4e7fd0233ae362cf9daa9316
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-08 14:40:11 -0700

    adds event listener on renderJewelry function

--- products/render-jewelry.js
+++ products/render-jewelry.js
@@ -1,3 +1,5 @@
+import findById from '../common/utils.js';
+
 function renderJewelry(jewelry) {
     const li = document.createElement('li');
     li.className = jewelry.category;
@@ -20,7 +22,35 @@ function renderJewelry(jewelry) {
 
     const button = document.createElement('button');
     button.textContent = 'Add to cart';
-    button.value = jewelry.code;
+    button.value = jewelry.id;
+
+    button.addEventListener('click', () => {
+        let cartItem = localStorage.getItem('CART');
+        let cart;
+        if (cartItem) {
+            cart = JSON.parse(cartItem);
+        } else {
+            cart = [];
+        }
+        let lineItem = findById(cart, jewelry.id);
+
+        if (!lineItem) {
+            lineItem = {
+                id: jewelry.id,
+                quantity: 1
+            };
+            cart.push(lineItem);
+        } else {
+            lineItem.quantity++;
+        }
+    
+        cartItem = JSON.stringify(cart);
+        localStorage.setItem('CART', cartItem);
+
+        alert('1 ' + jewelry.name + ' was added to your cart');
+    
+    });
+
     p.appendChild(button);
 
     li.appendChild(p);
