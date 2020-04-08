62e67ebdf6d93402913a3e5e641965316db0fc26
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-08 15:58:07 -0700

    bug fixes for linking html pages

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,5 +1,5 @@
 //Go get functions and arrays from imports
-import cart from '../Data/cart.js';
+// import cart from '../Data/cart.js';
 import jewelry from '../Data/product-data.js';
 import findById, { calcOrderTotal, toUSD } from '../common/utils.js';
 import { renderLineItem } from '../shopping-cart/render-line-item.js';
@@ -7,6 +7,18 @@ import { renderLineItem } from '../shopping-cart/render-line-item.js';
 // Locate on the DOM where the line items will go
 const tbody = document.querySelector('tbody');
 const orderTotalCell = document.getElementById('order-total-cell');
+// Bring in order total button from shopping-cart.html
+const orderButton = document.getElementById('order-button');
+
+// get shopping cart items from local storage 
+const json = localStorage.getItem('CART');
+let cart;
+// if there's something in the cart, make the string data into and object.  Otherwise, get an empty cart
+if (json) {
+    cart = JSON.parse(json);
+} else {
+    cart = [];
+}
 
 // Loop through cart items
 for (let i = 0; i < cart.length; i++) {
@@ -28,3 +40,18 @@ for (let i = 0; i < cart.length; i++) {
 const orderTotal = calcOrderTotal(cart, jewelry);
 //Update the order total cell's text content
 orderTotalCell.textContent = toUSD(orderTotal);
+
+// disable order button if there's no items in the cart.
+if (cart.length === 0) {
+    orderButton.disable = true;
+    // otheriwise, add an even listener that allows the order button to be clicked
+} else {
+    orderButton.addEventListener('click', () => {
+        // then remove cart items from local storage
+        localStorage.removeItem('CART');
+        //alert user to an order confirmation
+        alert('Confirmed!  Here is your order information: ' + JSON.stringify(cart, true, 2));
+        // take user back to home page
+        window.location = '../';
+    });
+}
